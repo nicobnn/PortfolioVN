5c66b8520430913adabbfe0f91ad1eccd53f35f3
Nicolas BAIN <nicolas.bain@edu.devinci.fr>
2020-03-04 17:12:39 +0100

    4th commit

--- index.js
+++ index.js
@@ -17,16 +17,20 @@ function add (a,b){
 return a+b
 }
 
-document.querySelector("#submit")
-.addEventListener("click", function writeToChat(event){
-
-    var message = document.querySelector("#message").value
-    var discussionElt = document.querySelector(".discussion")
-    discussionElt.innerHTML += "<p>" + message 
+function substraction(a, b) {
+    return a - b
+}
 
 
 
-})
+document.querySelector("#submit")//après #un id
+    .addEventListener("click",function writeToChat(event) {
+        var message = document.querySelector("#message").value
+        var discussionElt = document.querySelector(".discussion")//. c'est pour dire que c'est une classe
+        var nickname= document.querySelector("#nickname").value
+        
+        discussionElt.innerHTML += "<p>" + nickname + ": " + message
+    })
 
 
 //Les 7 types sur js
@@ -37,3 +41,12 @@ document.querySelector("#submit")
 //Undefined
 //Symbol
 //objet
+//ES2020 => BigInt
+
+//Pas de classe en js
+
+
+//camelCase     variable
+//PascalCase    fonction
+//kebab-case    
+//snake_case
