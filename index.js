f7d08be81e18fceebf5852db1150455ac72ce529
Nicolas BAIN <nicolas.bain@edu.devinci.fr>
2020-03-04 15:53:05 +0100

    Third commit

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 console.log('hello world');
-//Les 7 types sur js, le dernier a partir de juin 2020
+
+
+
+//pour declarer une variable
+var myNumber=42 // var n'a pas de tdz
+var otherNumber
+otherNumber=26
+const anotherNumber=282 // quand on met const on ne peut plus mettre var othernumber , other number = xxx
+let yetAnotherNumber=2323//la diff entre const et let , c'est que avec const on peut pas reaffecter
+//console.log(myNumber);
+
+
+//declaration d'une fonction
+
+function add (a,b){
+return a+b
+}
+
+document.querySelector("#submit")
+.addEventListener("click", function writeToChat(event){
+
+    var message = document.querySelector("#message").value
+    var discussionElt = document.querySelector(".discussion")
+    discussionElt.innerHTML += "<p>" + message 
+
+
+
+})
+
+
+//Les 7 types sur js
 //Number
 //String
 //Boolean
