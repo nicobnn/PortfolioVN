6304dfc01261cf1d7070f5441c6e4af222de7daa
Nicolas BAIN <nicolas.bain@edu.devinci.fr>
2020-03-18 13:59:51 +0100

    express 2nd commit

--- myapp/app.js
+++ myapp/app.js
@@ -8,3 +8,8 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.use(function (req, res, next) {
+    res.status(404).send("ERROR 404")
+  })
+//app.use(express.static('PortfolioVN/images/404.png'))
