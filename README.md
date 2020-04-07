9d5e4546d26b29ca7165eb392c5bb66f6dc809d9
Nicolas BAIN <nicolas.bain@edu.devinci.fr>
2020-04-07 18:38:54 +0200

    last commit

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is the website project of Nicolas BAIN Vincent MOUTEL for the Web Development course
 
-here is the link to the surge deployement : PortfolioVN-2020.surge.sh or portfoliovn.surge.sh
+here is the link to the surge deployement : portfoliovn.surge.sh
 
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('hello world');
+console.log('bienvenue sur portfoliovn');
 
 
 
@@ -20,33 +20,3 @@ return a+b
 function substraction(a, b) {
     return a - b
 }
-
-
-
-document.querySelector("#submit")//après #un id
-    .addEventListener("click",function writeToChat(event) {
-        var message = document.querySelector("#message").value
-        var discussionElt = document.querySelector(".discussion")//. c'est pour dire que c'est une classe
-        var nickname= document.querySelector("#nickname").value
-        
-        discussionElt.innerHTML += "<p>" + nickname + ": " + message
-    })
-
-
-//Les 7 types sur js
-//Number
-//String
-//Boolean
-//Null
-//Undefined
-//Symbol
-//objet
-//ES2020 => BigInt
-
-//Pas de classe en js
-
-
-//camelCase     variable
-//PascalCase    fonction
-//kebab-case    
-//snake_case
